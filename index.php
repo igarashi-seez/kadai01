54393a37f08b4b12aec31fb2020549f83bfdd30f
HiroshiIgarashi <tubukiti2008@gmail.com>
2023-01-17 23:30:35 +0900

    ファイルを整理

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php 
-var_dump($_POST);
+$post = $_POST;
+$key = $post['key'];
+$value = $post['value'];
+
+$json = file_get_contents("hiroshi.json");
+$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+$json_arr = json_decode($json,true);
+
+$json_arr += array (
+  $key => $value
+);
+
+$json_arr = json_encode($json_arr, JSON_UNESCAPED_UNICODE);
+file_put_contents("hiroshi.json", $json_arr);
--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-// const fs = require("fs");
-
 const writeKeyInput = document.getElementById('input01');
 const writeValueInput = document.getElementById('input02');
 const readKeyInput = document.getElementById('input03');
 const valueArea = document.getElementById('valueArea');
+const sendButton = document.getElementById('button01')
 const displayValueButton = document.getElementById('button02');
 const downloadButton = document.getElementById('button03');
+
+sendButton.addEventListener('click', function () {
+  const key = writeKeyInput.value;
+  const value = writeValueInput.value;
+  const data = 'key='+encodeURIComponent(key)+'&value='+encodeURIComponent(value);
+  const ajax = new XMLHttpRequest();
+  ajax.open('POST','index.php',true);
+  ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+  ajax.send(data);
+});
 
 displayValueButton.addEventListener('click', function () {
   displayValue(readKeyInput.value);
@@ -20,24 +29,10 @@ function displayValue (key) {
     valueArea.innerHTML = value;
   });
 }
+
 downloadButton.addEventListener('click', function () {
   let element = document.createElement('a');
   element.href = './hiroshi.json';
   element.download = 'hiroshi.json';
   element.click();
 })
-
-// 書き込むデータ準備
-let jsonData;
-let hiroshi;
-
-// fs.readFile("hiroshif.json", "utf-8", (err, data) => {
-//   if (err) throw err;
-//   jsonData = JSON.perse(data)
-// });
-
-// // 書き込み
-// fs.writeFile("file1.txt", data, (err) => {
-//   if (err) throw err;
-//   console.log('正常に書き込みが完了しました');
-// });
