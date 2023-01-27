533947c6cc8a9c5cf939f563eb053e0dbf0c0ade
HiroshiIgarashi <tubukiti2008@gmail.com>
2023-01-27 23:52:58 +0900

    テンプレートリテラルを用いた書き方に変更

--- main.js
+++ main.js
@@ -9,14 +9,18 @@ const downloadButton = document.getElementById('button03');
 
 //jsonに書き込み
 sendButton.addEventListener('click', function () {
-  const key = writeKeyInput.value;
-  const value = writeValueInput.value;
-  const data = 'key='+encodeURIComponent(key)+'&value='+encodeURIComponent(value);
+  sendData(writeKeyInput.value, writeValueInput.value)
+});
+
+function sendData(key, value) {
+  const encodedKey = encodeURIComponent(key);
+  const encodedValue = encodeURIComponent(value)
+  const data = `key=${encodedKey}&value=${encodedValue}`;
   const ajax = new XMLHttpRequest();
   ajax.open('POST','index.php',true);
   ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
   ajax.send(data);
-});
+}
 
 
 //jsonを読み込むためのイベントリスナー
